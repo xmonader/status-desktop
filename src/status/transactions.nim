6d5d9db6e686f1c308d1de16d892b51690108516
emizzle <eric.mastro@gmail.com>
2020-10-15 12:27:42 +1100

    fix: add some error handling to gas/gasPrice tx values

--- src/status/transactions.nim
+++ src/status/transactions.nim
@@ -2,18 +2,36 @@ import
   options, strutils
 
 import
-  stint, web3/ethtypes
+  stint, web3/ethtypes, chronicles
 
 import
   libstatus/types
 from libstatus/utils as status_utils import toUInt64, gwei2Wei, parseAddress
 
+logScope:
+  topics = "transactions"
+
 proc buildTransaction*(source: Address, value: Uint256, gas = "", gasPrice = ""): EthSend =
+  var
+    gasQty = Quantity.none
+    gasPriceInt = int.none
+  if not gas.isEmptyOrWhitespace:
+    try:
+      gasQty = Quantity(cast[uint64](parseFloat(gas).toUInt64)).some
+    except:
+      warn "Error parsing gas value", value=gas
+  
+  if not gasPrice.isEmptyOrWhitespace:
+    try:
+      gasPriceInt = gwei2Wei(parseFloat(gasPrice)).truncate(int).some
+    except:
+      warn "Error parsing gasPrice value", value=gasPrice
+    
   result = EthSend(
     source: source,
     value: value.some,
-    gas: (if gas.isEmptyOrWhitespace: Quantity.none else: Quantity(cast[uint64](parseFloat(gas).toUInt64)).some),
-    gasPrice: (if gasPrice.isEmptyOrWhitespace: int.none else: gwei2Wei(parseFloat(gasPrice)).truncate(int).some)
+    gas: gasQty,
+    gasPrice: gasPriceInt
   )
 
 proc buildTokenTransaction*(source, contractAddress: Address, gas = "", gasPrice = ""): EthSend =
